6bbd2804e3b10f31a43df7ff12781cf95afd693f
Fier <b5267667@gmail.com>
2026-06-09 21:39:01 +0300

    Update fspamer-new.lua

--- fspamer-new.lua
+++ fspamer-new.lua
@@ -5,26 +5,72 @@ local u8 = encoding.UTF8
 local ffi = require 'ffi'
 local requests = require 'requests'
 
-local current_version = 1.2
+local current_version = 1.3
 local url_version = "https://raw.githubusercontent.com/bogdanrazrab/Fspamer-updates/refs/heads/main/version.txt"
 local url_script = "https://raw.githubusercontent.com/bogdanrazrab/Fspamer-updates/refs/heads/main/fspamer-new.lua"
 
 local active = imgui.new.bool(false)
 local spamText = imgui.new.char[256](u8"текст")
-local delay = imgui.new.int(3)
+local delay = imgui.new.int(3) 
 local WinState = imgui.new.bool(false)
 
 local UpdateWinState = imgui.new.bool(false)
 local server_version_str = ""
 
+local function applyRedStyle()
+    local style = imgui.GetStyle()
+    local colors = style.Colors
+    
+    style.WindowRounding = 6.0
+    style.FrameRounding = 4.0
+    
+    colors[imgui.Col.WindowBg] = imgui.ImVec4(0.13, 0.10, 0.10, 1.00)
+    colors[imgui.Col.Header] = imgui.ImVec4(0.70, 0.15, 0.15, 0.80)
+    colors[imgui.Col.HeaderHovered] = imgui.ImVec4(0.85, 0.20, 0.20, 0.90)
+    colors[imgui.Col.HeaderActive] = imgui.ImVec4(0.95, 0.25, 0.25, 1.00)
+    
+    colors[imgui.Col.TitleBg] = imgui.ImVec4(0.50, 0.10, 0.10, 1.00)
+    colors[imgui.Col.TitleBgActive] = imgui.ImVec4(0.70, 0.12, 0.12, 1.00)
+    
+    colors[imgui.Col.Button] = imgui.ImVec4(0.65, 0.12, 0.12, 1.00)
+    colors[imgui.Col.ButtonHovered] = imgui.ImVec4(0.80, 0.18, 0.18, 1.00)
+    colors[imgui.Col.ButtonActive] = imgui.ImVec4(0.95, 0.25, 0.25, 1.00)
+    
+    colors[imgui.Col.FrameBg] = imgui.ImVec4(0.22, 0.15, 0.15, 1.00)
+    colors[imgui.Col.FrameBgHovered] = imgui.ImVec4(0.30, 0.18, 0.18, 1.00)
+    colors[imgui.Col.FrameBgActive] = imgui.ImVec4(0.40, 0.22, 0.22, 1.00)
+    
+    colors[imgui.Col.SliderGrab] = imgui.ImVec4(0.75, 0.15, 0.15, 1.00)
+    colors[imgui.Col.SliderGrabActive] = imgui.ImVec4(0.95, 0.25, 0.25, 1.00)
+    
+    colors[imgui.Col.CheckMark] = imgui.ImVec4(0.90, 0.15, 0.15, 1.00)
+end
+
+local styleInitialized = false
+
 imgui.OnFrame(function() return WinState[0] or UpdateWinState[0] end, function(player)
+    if not styleInitialized then
+        applyRedStyle()
+        styleInitialized = true
+    end
+
     if WinState[0] then
-        imgui.SetNextWindowSize(imgui.ImVec2(400, 200), imgui.Cond.FirstUseEver)
+        imgui.SetNextWindowSize(imgui.ImVec2(550, 280), imgui.Cond.FirstUseEver)
         
         if imgui.Begin("FSPAMER | Dev: Fier", WinState) then
-            if imgui.InputText(u8"Текст", spamText, 256) then end
-            if imgui.SliderInt(u8"Секунды", delay, 1, 60) then end
-            if imgui.Button(active[0] and u8"Стоп" or u8"Начать", imgui.ImVec2(-1, 40)) then
+            imgui.PushItemWidth(-1)
+            imgui.Text(u8"Введите текст для спама:")
+            if imgui.InputText("##SpamText", spamText, 256) then end
+            
+            imgui.Spacing()
+            imgui.Text(u8"Задержка (в секундах):")
+            if imgui.SliderInt("##DelaySeconds", delay, 1, 60) then end
+            imgui.PopItemWidth()
+            
+            imgui.SetCursorPosY(imgui.GetCursorPosY() + 20)
+            
+            local btnText = active[0] and u8"Остановить спам" or u8"Начать спам"
+            if imgui.Button(btnText, imgui.ImVec2(-1, 50)) then
                 active[0] = not active[0]
             end
             imgui.End()
@@ -32,7 +78,7 @@ imgui.OnFrame(function() return WinState[0] or UpdateWinState[0] end, function(p
     end
 
     if UpdateWinState[0] then
-        imgui.SetNextWindowSize(imgui.ImVec2(350, 140), imgui.Cond.Always)
+        imgui.SetNextWindowSize(imgui.ImVec2(400, 160), imgui.Cond.Always)
         imgui.SetNextWindowPos(imgui.ImVec2(imgui.GetIO().DisplaySize.x / 2, imgui.GetIO().DisplaySize.y / 2), imgui.Cond.Always, imgui.ImVec2(0.5, 0.5))
         
         if imgui.Begin(u8"Доступно обновление!", UpdateWinState, imgui.WindowFlags.NoResize + imgui.WindowFlags.NoCollapse) then
@@ -41,14 +87,14 @@ imgui.OnFrame(function() return WinState[0] or UpdateWinState[0] end, function(p
             imgui.Separator()
             imgui.Spacing()
             
-            if imgui.Button(u8"Да, обновить", imgui.ImVec2(140, 35)) then
+            if imgui.Button(u8"Да, обновить", imgui.ImVec2(160, 40)) then
                 UpdateWinState[0] = false
                 downloadUpdate()
             end
-            imgui.SameLine(180)
-            if imgui.Button(u8"Пропустить", imgui.ImVec2(140, 35)) then
+            imgui.SameLine(220)
+            if imgui.Button(u8"Пропустить", imgui.ImVec2(160, 40)) then
                 UpdateWinState[0] = false
-                sampAddChatMessage("{3399FF}[FSPAMER]{FFFFFF} Обновление пропущено пользователем.", -1)
+                sampAddChatMessage("{FF3333}[FSPAMER]{FFFFFF} Обновление пропущено пользователем.", -1)
             end
             imgui.End()
         end
@@ -72,14 +118,14 @@ end
 
 function downloadUpdate()
     lua_thread.create(function()
-        sampAddChatMessage("{3399FF}[FSPAMER]{FFFFFF} Скачивание новой версии...", -1)
+        sampAddChatMessage("{FF3333}[FSPAMER]{FFFFFF} Скачивание новой версии...", -1)
         local script_status, script_response = pcall(requests.get, url_script)
         if script_status and script_response.status_code == 200 then
             local file = io.open(thisScript().path, "wb")
             if file then
                 file:write(script_response.text)
                 file:close()
-                sampAddChatMessage("{3399FF}[FSPAMER]{FFFFFF} Скрипт успешно обновлен! Перезагрузка...", -1)
+                sampAddChatMessage("{FF3333}[FSPAMER]{FFFFFF} Скрипт успешно обновлен! Перезагрузка...", -1)
                 thisScript():reload()
             else
                 sampAddChatMessage("{FF3333}[FSPAMER]{FFFFFF} Ошибка: не удалось открыть локальный файл для записи.", -1)
@@ -90,13 +136,29 @@ function downloadUpdate()
     end)
 end
 
+function runSpammer()
+    lua_thread.create(function()
+        while true do
+            wait(0)
+            if active[0] then
+                local text = u8:decode(ffi.string(spamText))
+                if text ~= "" then
+                    sampSendChat(text)
+                end
+                wait(delay[0] * 1000)
+            end
+        end
+    end)
+end
+
 function main()
     while not isSampAvailable() do wait(100) end
 
-    sampAddChatMessage("{3399FF}[FSPAMER]{FFFFFF} Скрипт загружен. Автор: {3399FF}Fier", -1)
-    sampAddChatMessage("{3399FF}[FSPAMER]{FFFFFF} Активация меню: {3399FF}/fpanel", -1)
+    sampAddChatMessage("{FF3333}[FSPAMER]{FFFFFF} Скрипт загружен. Автор: {FF3333}Fier", -1)
+    sampAddChatMessage("{FF3333}[FSPAMER]{FFFFFF} Активация меню: {FF3333}/fpanel", -1)
     
     checkUpdates()
+    runSpammer()
     
     sampRegisterChatCommand("fpanel", function() 
         WinState[0] = not WinState[0] 
@@ -105,13 +167,5 @@ function main()
     while true do
         wait(0)
         imgui.ShowCursor = WinState[0] or UpdateWinState[0]
-        
-        if active[0] then
-            local text = u8:decode(ffi.string(spamText))
-            if text ~= "" then
-                sampSendChat(text)
-            end
-            wait(delay[0] * 1000)
-        end
     end
 end
